4230506a76846b69edb6f3c3a35fd91055047a40
Lokanath <lokeshlokanath9@gmail.com>
2026-01-03 23:11:01 +0530

    updated the code

--- cognify tasks/Level 1/Task 1.py
+++ cognify tasks/Level 1/Task 1.py
@@ -1,7 +1,5 @@
 def reverse_string(s):
     return s[ ::-1]
-
-# Example usage
 input_string = input("enter the string: ")
 output_string = reverse_string(input_string)
-print(output_string)
+print("reverse of the input_string: ",output_string)
