c0b95e6d665a607a49d68de4f77f661118d2af8b
Lokanath <lokeshlokanath9@gmail.com>
2026-01-09 19:39:40 +0530

    Update Task4 code

--- cognify tasks/Level 2/Task1.py
+++ cognify tasks/Level 2/Task1.py
@@ -9,9 +9,9 @@ while True:
     guess = int(input("Enter your guess: "))
 
     if guess < secret_number:
-        print("Too low")
+        print("Too low, enter the high number")
     elif guess > secret_number:
-        print("Too high")
+        print("Too high, enter the low number")
     else:
         print("Congratulations! You guessed the correct number.")
         break
